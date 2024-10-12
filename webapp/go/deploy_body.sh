cb9ba8ed29457414f36277de9561aadffecf4738
cyrill <1283323+t-cyrill@users.noreply.github.com>
2024-10-12 14:59:13 +0900

    sqlをデプロイできるように

--- webapp/go/deploy_body.sh
+++ webapp/go/deploy_body.sh
@@ -7,6 +7,7 @@ GOOS=linux GOARCH=amd64 go build -o isucari_linux
 for server in isu01; do
   ssh -t $server "sudo systemctl stop isucari.golang.service"
   scp ./isucari_linux $server:/home/isucon/isucari/webapp/go/isucari
+  rsync -vau ../sql/ $server:/home/isucon/isucari/webapp/sql/
   ssh -t $server "sudo systemctl start isucari.golang.service"
 done
 
